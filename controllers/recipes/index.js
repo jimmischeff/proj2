c007248562adf790104c16ed238bc99735402fef
James Scheffler <jimmischeff@gmail.com>
2020-10-02 10:59:01 -0400

    website now requires user to be logged in to access

--- controllers/recipes/index.js
+++ controllers/recipes/index.js
@@ -17,7 +17,7 @@ const router = Router();
 //TEST ROUTE TO SHOW HOW AUTH MIDDLEWARE WORKS
 
 //INDEX
-router.get('/', (req, res) => {
+router.get('/', auth, (req, res) => {
   recipes.find({}, (err, recipe) => {
     res.render('index.jsx', {
       recipes: recipe
@@ -26,41 +26,41 @@ router.get('/', (req, res) => {
 })
 
 //NEW
-router.get("/new", (req, res) => {
+router.get("/new", auth, (req, res) => {
   res.render('new.jsx', {
   });
 });
 
 //DESTROY
-router.delete('/:id', (req, res) => {
+router.delete('/:id', auth, (req, res) => {
   recipes.findByIdAndRemove(req.params.id, (err, data) => {
     res.redirect('/recipes')
   })
 })
 
 //UPDATE
-router.put('/:id', (req, res) => {
+router.put('/:id', auth, (req, res) => {
   recipes.findByIdAndUpdate(req.params.id, req.body, {new: true}, (err, updateModel) => {
     res.redirect('/recipes')
   })
 })
 
 //CREATE
-router.post('/', (req, res) => {
+router.post('/', auth, (req, res) => {
   recipes.create(req.body)
     res.redirect('/recipes')
 
 })
 
 //EDIT
-router.get('/:id/edit', (req, res) => {
+router.get('/:id/edit', auth, (req, res) => {
   recipes.findById(req.params.id, (err, foundRecipe) => {
     res.render('edit.jsx', {recipe: foundRecipe})
   })
 })
 
 //SHOW
-router.get("/:id", (req, res) => {
+router.get("/:id", auth, (req, res) => {
   recipes.findById(req.params.id, (err, foundRecipe) => {
     res.render('show.jsx', {recipe: foundRecipe})
   })
